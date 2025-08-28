9360a54fd1f5fdc9fc66e5bf99d46773e25ea287
Ubuntu <ubuntu@tkt-calvin-0.arbutus>
2025-08-28 14:23:54 +0000

    one more CPU_SET deleted

--- src/scheduler/deterministic_scheduler.cc
+++ src/scheduler/deterministic_scheduler.cc
@@ -52,9 +52,9 @@ DeterministicScheduler::DeterministicScheduler(ClusterConfig* conf,
   cpu_set_t cpuset;
   pthread_attr_t attr1;
   pthread_attr_init(&attr1);
-  CPU_ZERO(&cpuset);
-  CPU_SET(5, &cpuset);
-  pthread_attr_setaffinity_np(&attr1, sizeof(cpu_set_t), &cpuset); 
+  //CPU_ZERO(&cpuset);
+ // CPU_SET(5, &cpuset);
+  //pthread_attr_setaffinity_np(&attr1, sizeof(cpu_set_t), &cpuset); 
   pthread_create(&lock_manager_thread_, &attr1, LockManagerThread, reinterpret_cast<void*>(this));
 
   // Start all worker threads.
